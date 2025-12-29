25cb8ca460d7974959960abc1206832975be0810
shijas <jas8848918@gmail.com>
2025-12-29 09:59:19 +0530

    initial migration pk

--- ams_pk_uuid/models.py
+++ ams_pk_uuid/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
-# Create your models here.
+# “first step 1 migration normal added the pk field migration”
+class AppointmentUser(models.Model):
+    appointmentUser_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=25)
+    phone = models.CharField(max_length=12, blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Company(models.Model):
+    company_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    discription = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Event(models.Model):
+    event_id = models.AutoField(primary_key=True)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
+
+
+class Slot(models.Model):
+    STATUS_CHOICES = (
+        ("booked", "Booked"),
+        ("open", "Open"),
+    )
+    slot_id = models.AutoField(primary_key=True)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, null=True)
+    start_time = models.TimeField(null=True)
+    end_time = models.TimeField(null=True)
+    date = models.DateField(null=True)
+    user = models.ForeignKey(AppointmentUser, on_delete=models.CASCADE, null=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default="open")
